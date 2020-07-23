f06c8f5ff4d32968fa63accd1decca90e9ab2cda
LinusCebulla1208 <59658701+LinusCebulla1208@users.noreply.github.com>
2020-07-23 12:02:57 +0200

    Methode entfernt
    Es wurde eine Methode entfernt die nur für einen Test da war.

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ def loop(screen):
     cont = True
 
     pfad = parse_path("M 8,30A 20,20 0,0,1 50,30A 20,20 0,0,1 90,30Q 90,60 50,90Q 10,60 10,30 z")
+
     vis.animSVG(screen, (255, 255, 255), pfad, (6, 6), (0, 0), 2, 3, FRAMERATE, clock)
+    line = vis.animLine(screen, (255, 255, 255), (100, 100), (200, 300), 3, 1, 60, clock)
 
     while cont:
         for event in pg.event.get(): #Wenn Fenster geschlossen --> Main Loop beenden
@@ -31,19 +33,12 @@ def loop(screen):
 
         #--ZEICHENVORGANG
         vis.drawSVG(screen, (255, 255, 255), pfad, (6, 6), (0, 0), 0.01, 2)
+        pg.draw.rect(screen, (255, 255, 255), line)
 
         #Fenster updaten
         pg.display.flip()
 
         #max. 60fps
         clock.tick(FRAMERATE)
-
-def anim_line(screen, clock):
-    for i in np.arange(0, 1, 0.01):
-        screen.fill((0, 0, 0))
-        pg.draw.line(screen, (255, 255, 255), (100, 100), (100*(1+i), 100*(1+i)), 2)
-        pg.display.flip()
-        clock.tick(60)
-
 
 main()
